7616241f3ad82b7eb5a52e2ff3ceae14101a3b8f
Alexander Nissen <alexander@feldbech.dk>
2021-05-11 20:40:23 +0200

    Add a bit of sanitization

--- scripts/writeup_gen.py
+++ scripts/writeup_gen.py
@@ -1,6 +1,7 @@
 from datetime import date
 import pathlib
 import templates
+import urllib
 
 
 ROOT_DIR = pathlib.Path(__file__).resolve().parents[1]
@@ -16,8 +17,19 @@ def make_dir(path):
         dir_path.mkdir(parents=True)
 
 
+def sanitize(path):
+    return path.replace(" ", "-").replace("?", "").replace("/", "_")
+
+
+def yaml_escape(str):
+    for c in "{}[],&:*#?|-<>=!%@\\`":
+        if c in str:
+            return f">-\n    {str}"
+    return str
+
+
 def create_ctf_index(ctf_name):
-    ctf_path = ctf_name.replace(" ", "-")
+    ctf_path = sanitize(ctf_name)
     make_dir(ASSET_DIR / ctf_path)
 
     if pathlib.Path(WRITEUP_DIR / ctf_path).exists():
@@ -25,23 +37,24 @@ def create_ctf_index(ctf_name):
         return
 
     make_dir(WRITEUP_DIR / ctf_path)
-    
-    index = templates.ctf_overview.format(ctf_name, date.today())
     with open(WRITEUP_DIR / ctf_path / "index.md", "w+") as fout:
-        fout.write(index)
+        fout.write(templates.ctf_overview.format(
+            yaml_escape(ctf_name),
+            date.today()
+        ))
 
 
 def create_chall(ctf_name, chall_name, points, solves, tags):
     tag_list = tags.replace(", ", ",").split(",")
     formatted_tags = "".join([f"\n    - {tag}" for tag in tag_list])
     
-    ctf_path = ctf_name.replace(" ", "-")
-    chall_path = chall_name.replace(" ", "-")
+    ctf_path = sanitize(ctf_name)
+    chall_path = sanitize(chall_name)
     writeup_file = WRITEUP_DIR / ctf_path / (chall_path + ".md")
     with open(writeup_file, "w+") as fout:
         fout.write(templates.ctf_writeup.format(
-            chall_name,
-            ctf_name,
+            yaml_escape(chall_name),
+            yaml_escape(ctf_name),
             points,
             solves,
             formatted_tags,
